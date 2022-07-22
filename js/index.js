219618e9610ef331d30af545cf9408c2f57d05b5
Charles Lim <charleslimjh@u.nus.edu>
2022-07-22 14:59:08 +0800

    Reset password feature added to index.html

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase
 import {
   getAuth,
   signInWithEmailAndPassword,
+  sendPasswordResetEmail
 } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
 import {
   getFirestore,
@@ -63,3 +64,15 @@ form.addEventListener("submit", function () {
       alert("Invalid account credentials!");
     });
 });
+
+// Forget password handler
+const forgetForm = document.getElementById('forgetPassForm');
+forgetForm.addEventListener('submit', function() {
+  const forgetEmail = document.getElementById("forgetPassEmail").value;
+  sendPasswordResetEmail(auth, forgetEmail).then(() => {
+    alert("Password reset link has been sent to your email. Please check your email (especially your spam folder!) for further instructions.");
+    window.location.replace('index.html');
+  }).catch((error) => {
+    alert("User not found in our database. Please sign up for an account instead.");
+  })
+})
